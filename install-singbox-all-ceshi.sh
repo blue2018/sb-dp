f0b35f7b3b3750c05376403f130ab1245488dc03
caigouzi121380 <158837412+caigouzi121380@users.noreply.github.com>
2025-11-08 19:38:00 +0800

    Update install-singbox-all-ceshi.sh

--- install-singbox-all-ceshi.sh
+++ install-singbox-all-ceshi.sh
@@ -282,7 +282,7 @@ create_config() {
     {
       "type": "hysteria2",
       "tag": "hy2-in",
-      "listen": "0.0.0.0",
+      "listen": "::",
       "listen_port": $PORT_HY2,
       "users": [
         {
