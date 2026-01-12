76f9ae3bfeb4f4355abb06579f83c21a5736956a
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-12 20:32:32 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -567,9 +567,7 @@ create_config() {
     fi
     [ -z "$SALA_PASS" ] && SALA_PASS=$(openssl rand -base64 16 | tr -dc 'a-zA-Z0-9' | head -c 16)
 
-    local timeout="30s"
-    local mem_total=$(probe_memory_total)
-    mem_total=${mem_total:-64}
+    local mem=$(probe_memory_total); : ${mem:=512}; local timeout="30s"
     [ "$mem" -ge 450 ] && timeout="60s" || { [ "$mem" -ge 200 ] && timeout="50s"; } || { [ "$mem" -ge 100 ] && timeout="40s"; }
     # 4. 写入 Sing-box 配置文件
     cat > "/etc/sing-box/config.json" <<EOF
