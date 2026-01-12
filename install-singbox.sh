463d6839e7c8417c750244edb1fdc7964824d82c
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-12 09:59:56 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -583,7 +583,6 @@ create_config() {
     "udp_timeout": "$timeout",
     "udp_fragment": true,
     "tcp_fast_open": true,
-    "tcp_multi_path": false,
     "disable_tcp_keep_alive": true,
     "tls": {"enabled": true, "alpn": ["h3"], "certificate_path": "/etc/sing-box/certs/fullchain.pem", "key_path": "/etc/sing-box/certs/privkey.pem"},
     "obfs": {"type": "salamander", "password": "$SALA_PASS"},
