a09d5215b772343d8adff5b28cac4307d7bee944
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-15 23:22:34 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -316,22 +316,7 @@ safe_rtt() {
 }
 
 # sing-box 用户态运行时调度人格（Go/QUIC/缓冲区自适应）
-apply_userspace_adaptive_profile() {  
-        
-        
-          
-            
-          
-          重试
-          
-        
-          
-            
-          
-          错误原因
-        
-        
-        
+apply_userspace_adaptive_profile() {
     local g_procs="$1" wnd="$2" buf="$3" real_c="$4" mem_total="$5"
 	export GOGC="${SBOX_GOGC:-100}" GOMEMLIMIT="${SBOX_GOLIMIT:-48MiB}" GOMAXPROCS="$g_procs" GODEBUG="madvdontneed=1"
     # === 1. GOMAXPROCS 智能调整 ===
