49a2d381edf7c0428e059a5f7f56b460c5c31756
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-18 12:21:37 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -442,8 +442,8 @@ optimize_system() {
         SBOX_OPTIMIZE_LEVEL="128M 紧凑版"
     else
         VAR_HY2_BW="150"; max_udp_mb=$((mem_total * 55 / 100))
-        SBOX_GOLIMIT="$((mem_total * 65 / 100))MiB"; SBOX_GOGC="100"
-		SBOX_MEM_HIGH="$((mem_total * 75 / 100))M"; SBOX_MEM_MAX="$((mem_total * 85 / 100))M"
+        SBOX_GOLIMIT="$((mem_total * 70 / 100))MiB"; SBOX_GOGC="100"
+		SBOX_MEM_HIGH="$((mem_total * 80 / 100))M"; SBOX_MEM_MAX="$((mem_total * 90 / 100))M"
         VAR_SYSTEMD_NICE="-5"; VAR_SYSTEMD_IOSCHED="best-effort"; tcp_rmem_max=4194304  
         g_procs=1; swappiness_val=100; busy_poll_val=0; ct_max=16384; ct_stream_to=30
         SBOX_OPTIMIZE_LEVEL="64M 激进版"
