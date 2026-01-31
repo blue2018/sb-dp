159f97bb19e7644ea1c379daf8b872ba9868563c
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-31 14:44:33 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -853,14 +853,12 @@ display_links() {
     local BASE_PARAM="sni=$RAW_SNI&alpn=h3&insecure=1"
     [ -n "${RAW_FP:-}" ] && BASE_PARAM="${BASE_PARAM}&pinsha256=${RAW_FP}"
     [ -n "${RAW_SALA:-}" ] && BASE_PARAM="${BASE_PARAM}&obfs=salamander&obfs-password=${RAW_SALA}"
-
     get_status_label() {
         [ -z "$1" ] || ! command -v nc >/dev/null 2>&1 && return
         nc -z -u -w 2 "$1" "$RAW_PORT" >/dev/null 2>&1 && \
         echo -e " \033[1;32m(已连通)\033[0m" || echo -e " \033[1;33m(本地受阻)\033[0m"
     }
-	
-    echo -e "\n\033[1;32m[节点信息]\033[0m \033[1;34m>>>\033[0m 运行端口: \033[1;33m${RAW_PORT:-"未知"}\033[0m"
+    echo -e "\n\033[1;32m[节点信息]\033[0m \033[1;34m>>>\033[0m 运行端口: \033[1;33m${RAW_PORT:-"未知"}\033[0m\n"
     [ -n "${RAW_IP4:-}" ] && {
         LINK_V4="hy2://$RAW_PSK@$RAW_IP4:$RAW_PORT/?${BASE_PARAM}#$(hostname)_v4"
         local v4_status=$(get_status_label "$RAW_IP4")
