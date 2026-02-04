73e6d2a400fa2dd63be06b20f9d9c8ef38cfbe50
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-02-04 13:01:56 +0800

    Update sb_hy2.sh

--- sb_hy2.sh
+++ sb_hy2.sh
@@ -444,7 +444,7 @@ optimize_system() {
         [ "$real_c" -gt 2 ] && g_procs=2 || g_procs=$real_c
         SBOX_OPTIMIZE_LEVEL="128M 紧凑版"
     else
-        VAR_HY2_BW="130"; max_udp_mb=$((mem_total * 53 / 100))
+        VAR_HY2_BW="130"; max_udp_mb=$((mem_total * 56 / 100))
         SBOX_GOLIMIT="$((mem_total * 70 / 100))MiB"; SBOX_GOGC="100"
         SBOX_MEM_HIGH="$((mem_total * 80 / 100))M"; SBOX_MEM_MAX="$((mem_total * 90 / 100))M"
         VAR_SYSTEMD_NICE="-5"; VAR_SYSTEMD_IOSCHED="best-effort"; tcp_rmem_max=2097152
