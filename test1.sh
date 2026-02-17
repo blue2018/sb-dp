a130775a9d3b3d1c99b30b7f9027af66fe1931a4
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-02-17 13:30:34 +0800

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -697,11 +697,11 @@ create_config() {
     [ "$mem_total" -ge 100 ] && timeout="40s"; [ "$mem_total" -ge 200 ] && timeout="60s"; [ "$mem_total" -ge 450 ] && timeout="80s"
     
     local dns_srv='{"address":"8.8.4.4","detour":"direct-out"},{"address":"1.1.1.1","detour":"direct-out"}'
-    local sniffing=''; local dns_out=''; local route_rule=''
+    local sniffing=''; local route_rule=''
     
     if [ "$mem_total" -ge 100 ]; then
         dns_srv='{"tag":"google-doh","address":"https://8.8.4.4/dns-query","detour":"direct-out"},{"tag":"cloudflare-doh","address":"https://1.1.1.1/dns-query","detour":"direct-out"}'
-        sniffing=', "sniff_override": ["http", "tls", "quic"], "sniff_timeout": "300ms"'
+        sniffing=', "sniff_enabled": true, "sniff_timeout": "300ms", "sniff_override_destination": true'
         route_rule=', "route": { "rules": [ { "protocol": "dns", "action": "hijack-dns" } ] }'
     fi
     
