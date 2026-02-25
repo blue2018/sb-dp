7b665426509196efa28e2f3732a8330071b68a49
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-02-25 10:15:56 +0800

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -877,8 +877,8 @@ EOF
 	    pkill -9 cloudflared >/dev/null 2>&1 || true
 	    local cf_memlimit; [ "${mem_total:-64}" -ge 256 ] && cf_memlimit="40MiB" || cf_memlimit="30MiB"
 	    GOGC=30 GOMEMLIMIT=${cf_memlimit} GOMAXPROCS="${CPU_CORE:-1}" nohup /usr/local/bin/cloudflared tunnel \
-	        --protocol auto --edge-ip-version auto --no-autoupdate --heartbeat-interval 10s --heartbeat-count 2 \
-	        run --token "${ARGO_TOKEN}" >/dev/null 2>&1 &
+		    --protocol quic --edge-ip-version auto --no-autoupdate --heartbeat-interval 10s --heartbeat-count 2 \
+		    run --post-quantum --token "${ARGO_TOKEN}" >/dev/null 2>&1 &
 	fi
     if [ -n "$pid" ] && [ -e "/proc/$pid" ]; then
         local ma=$(awk '/^MemAvailable:/{a=$2;f=1} /^MemFree:|Buffers:|Cached:/{s+=$2} END{print (f?a:s)}' /proc/meminfo 2>/dev/null)
