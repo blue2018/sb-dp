f4eb00ff1bab48b1edb2e67326c13367522daef4
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-02-25 14:28:06 +0800

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -876,7 +876,7 @@ EOF
     if [ "${USE_EXTERNAL_ARGO:-false}" = "true" ] && [ -n "${ARGO_TOKEN:-}" ]; then
 	    pkill -9 cloudflared >/dev/null 2>&1 || true
 	    local cf_memlimit; [ "${mem_total:-64}" -ge 256 ] && cf_memlimit="40MiB" || cf_memlimit="30MiB"
-	    GOGC=60 nohup /usr/local/bin/cloudflared tunnel \
+	    GOGC=30 GOMEMLIMIT=${cf_memlimit} GOMAXPROCS="${CPU_CORE:-1}" nohup /usr/local/bin/cloudflared tunnel \
 		    --protocol quic --edge-ip-version auto --no-autoupdate --heartbeat-interval 10s --heartbeat-count 2 \
 		    run --post-quantum --token "${ARGO_TOKEN}" >/dev/null 2>&1 &
 	fi
